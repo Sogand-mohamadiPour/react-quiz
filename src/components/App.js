698f11ace789571d22f2c92c243a5f07b6d03a70
Sogand <sogandmohamadipour7@gmail.com>
2025-11-18 22:49:59 +0330

    next question and next btn

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Loader from "./Loader"
 import Error from "./Error"
 import StartScreen from "./StartScreen";
 import Questions from "./Questions";
+import NextButton from "./NextButton";
 
 const initialState = {
   questions: [],
@@ -44,6 +45,9 @@ function reducer(state, action) {
         points: action.payload === question.correctOption ? state.points + question.points : state.points,
       };
 
+    case 'nextQuestion':
+      return { ...state, index: state.index + 1, answer: null };
+
     default:
       throw new Error("action unknown")
   }
@@ -72,11 +76,16 @@ export default function App() {
             numQuestions={numQuestions}
             dispatch={dispatch}
           />}
-          {status === 'active' && <Questions
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />}
+          {status === 'active' && (
+            <>
+              <Questions
+                question={questions[index]}
+                dispatch={dispatch}
+                answer={answer}
+              />
+              <NextButton dispatch={dispatch} answer={answer} />
+            </>
+          )}
         </Main>
       </div>
     </>
