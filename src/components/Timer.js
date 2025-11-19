deb92fdb1521ba6fc75ce17322d6f2445920ed48
Sogand <sogandmohamadipour7@gmail.com>
2025-11-19 22:08:32 +0330

    timer completed

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react"
 
 export default function Timer({ dispatch, secondsRemaining }) {
+    const mins = Math.floor(secondsRemaining / 60);
+    const seconds = secondsRemaining % 60;
 
     useEffect(function () {
         const id = setInterval(function () {
@@ -13,7 +15,9 @@ export default function Timer({ dispatch, secondsRemaining }) {
     return (
         <>
             <div className="timer">
-                {secondsRemaining}
+                {mins < 10 && '0'}
+                {mins}:{seconds < 10 && '0'}
+                {seconds}
             </div>
         </>
     )
