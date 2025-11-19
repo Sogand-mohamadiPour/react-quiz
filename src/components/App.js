2a71eb3c12bef1ee900c53ec5bba7ba4cf9bda22
Sogand <sogandmohamadipour7@gmail.com>
2025-11-19 21:49:33 +0330

    adding a timer

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,8 @@ import Questions from "./Questions";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
 import FinishedScreen from "./FinishedScreen";
+import Timer from "./Timer";
+import Footer from "./Footer";
 
 const initialState = {
   questions: [],
@@ -18,6 +20,7 @@ const initialState = {
   answer: null,
   points: 0,
   highscore: Number(localStorage.getItem('highscore')) || 0,
+  secondsRemaining: 10,
 }
 
 function reducer(state, action) {
@@ -74,13 +77,21 @@ function reducer(state, action) {
         status: "ready"
       }
 
+    case 'tick':
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      }
+
+
     default:
       throw new Error("action unknown");
   }
 };
 
 export default function App() {
-  const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points, highscore, secondsRemaining }, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
   const maxPossiblePoints = questions.reduce((prev, cur) =>
@@ -118,12 +129,18 @@ export default function App() {
                 dispatch={dispatch}
                 answer={answer}
               />
-              <NextButton
-                dispatch={dispatch}
-                answer={answer}
-                index={index}
-                numQuestions={numQuestions}
-              />
+              <Footer>
+                <Timer
+                  dispatch={dispatch}
+                  secondsRemaining={secondsRemaining}
+                />
+                <NextButton
+                  dispatch={dispatch}
+                  answer={answer}
+                  index={index}
+                  numQuestions={numQuestions}
+                />
+              </Footer>
             </>
           )}
           {status === 'finished' && (<FinishedScreen
