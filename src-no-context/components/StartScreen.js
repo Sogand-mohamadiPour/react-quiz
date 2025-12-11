18e014e410bdc36b7822e1682455b6e860fba319
Sogand <sogandmohamadipour7@gmail.com>
2025-12-11 22:56:15 +0330

    adding a new src

---
+++ src-no-context/components/StartScreen.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function StartScreen({ numQuestions, dispatch }) {
+    return (
+        <>
+            <div className='start'>
+                <h3>Welcome to React Quiz!</h3>
+                <h3>{numQuestions} questions to test your React mastry</h3>
+                <button
+                    className='btn btn-ui'
+                    onClick={() => dispatch({ type: "start" })}
+                >
+                    Let's start
+                </button>
+            </div>
+        </>
+    )
+}
